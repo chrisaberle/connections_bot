ce63e8f4e94bfa21688307771bb407ccabc7bda2
Chris Aberle <35345242+chrisaberle@users.noreply.github.com>
2023-10-14 16:46:27 -0500

    Fiddling with deployment details

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import logging
 import aiohttp
 import re
 import asyncio
+import uvicorn
 
 
 # Configure logging
@@ -541,12 +542,12 @@ def get_user_list():
 
     return users_df
 
-
-# Main entry point
-if __name__ == "__main__":
-    try:
-        port = os.environ.get("PORT", "5000")
-        port = int(port)
-    except ValueError:
-        port = 5000
-    uvicorn.run("main:app", host='0.0.0.0', port=port, log_level="info")
+# Deprecated in favor of Procfile once moved to hosting evironment
+# # Main entry point
+# if __name__ == "__main__":
+#     try:
+#         port = os.environ.get("PORT", "5000")
+#         port = int(port)
+#     except ValueError:
+#         port = 5000
+#     uvicorn.run("main:app", host='0.0.0.0', port=port, log_level="info")
